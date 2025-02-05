b1534f73f608b92b5aca969c75984f647513cc91
akash kanojiya <akashkanojiya307@gmail.com>
2025-02-05 18:53:56 +0530

    commit

--- src/Components/BusinessCentric.jsx
+++ src/Components/BusinessCentric.jsx
@@ -5,7 +5,7 @@ function BusinessCentric() {
   return (
     <div className="px-2  py-14">
     
-      <div className="flex flex-col sm:flex-row-reverse px-6 sm:px-12 items-center justify-center sm:justify-evenly rounded-xl py-16 mx-6 my-10 bg-white shadow-2xl absolute top-10 left-10 w-36 h-36 sm:w-72 sm:h-56 bg-pink-400 opacity-20 rounded-full filter blur-3xl animate-pulse absolute bottom-10 right-10 w-48 h-48 sm:w-96 sm:h-56 bg-red-400 opacity-20 rounded-full filter blur-3xl animate-pulse">
+      <div className="flex flex-col sm:flex-row-reverse px-6 sm:px-12 items-center justify-center sm:justify-evenly rounded-xl py-16 mx-6 my-10 bg-white shadow-2xl  ">
       
         <div className="sm:w-1/2 py-4  sm:text-left text-black">
           <p className="text-4xl font-train pb-4   text-[#dc2626] bg-clip-text relative group">
@@ -18,10 +18,8 @@ function BusinessCentric() {
 
           </p>
  
-          {/* <button className="mt-6 bg-gradient-to-r from-red-400 to-customBlue text-white px-6 py-2 rounded-full font-semibold text-lg transition-all duration-300 hover:bg-gradient-to-l hover:from-customBlue  hover:to-red-500 shadow-lg hover:scale-105">
-          <a href="#"> Join Us Now</a>
+         
            
-          </button> */}
         </div>
         <div className="sm:w-2/5 sm:mt-0 mt-8 flex justify-center relative">
           <div className="absolute inset-0  opacity-20 rounded-lg blur-sm"></div>
